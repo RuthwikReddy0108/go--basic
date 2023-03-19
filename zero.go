71eb4eb1c3ced9ad2312be2433113898951eeca0
Ruthwikreddy <120026571+RuthwikReddy0108@users.noreply.github.com>
2023-03-19 12:02:39 +0530

    Create zero.go

---
+++ zero.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	var i int
+	var f float64
+	var b bool
+	var s string
+	var arr []int // new variable declared
+	fmt.Printf("%v %v %v %q %v\n", i, f, b, s, arr)
+}
