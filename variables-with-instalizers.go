4a0b51a82c80d89cedebd33bc662f9870917b38c
Ruthwikreddy <120026571+RuthwikReddy0108@users.noreply.github.com>
2023-03-19 11:55:28 +0530

    Create variables-with-instalizers.go

---
+++ variables-with-instalizers.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+var i, j int = 1, 2
+
+func main() {
+	var c, python, java = true, false, "no!"
+	k := 5 // declaring and initializing a new variable using the short variable declaration syntax
+	fmt.Println(i, j, c, python, java, k)
+}
