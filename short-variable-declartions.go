c90f7924e60d1b386e62e50c5221963e652051a2
Ruthwikreddy <120026571+RuthwikReddy0108@users.noreply.github.com>
2023-03-19 11:58:59 +0530

    Create short-variable-declartions.go

---
+++ short-variable-declartions.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	var i, j int = 1, 2
+	k := 3
+	c, python, java := true, false, "no!"
+	var message string = "Hello, love!"
+
+	fmt.Println(i, j, k, c, python, java, message)
+}
